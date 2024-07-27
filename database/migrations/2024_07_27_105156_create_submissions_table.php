10f450f622d31852e18495778ab0dc91b831953a
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-07-28 01:50:37 +0800

    feat(submission): create submission for admin and student

---
+++ database/migrations/2024_07_27_105156_create_submissions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('student_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['submitted', 'pending', 'proses_kajur', 'proses_dekan', 'done'])->default('submitted');
+            $table->text('note')->nullable();
+            $table->string('file_result')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('submission_files', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('submission_id')->constrained()->onDelete('cascade');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submission_files');
+        Schema::dropIfExists('submissions');
+    }
+};
