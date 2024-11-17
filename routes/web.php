b155fad28c38c314d6c0f2012a028244748c5d80
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-11-17 17:31:02 +0800

    fix: route home in web.php

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
     } else {
         abort(403);
     }
-});
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
